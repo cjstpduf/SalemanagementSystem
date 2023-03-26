f8ae650668a7f8c4e30457179b1e8c4d790455c2
tpduf <tpduf@tpduf>
2023-03-26 16:20:14 +0900

    added Sale.java

--- src/git_SalemanagementSystem/Sale.java
+++ src/git_SalemanagementSystem/Sale.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class Sale{
 	public static void main(String[] args) {
-	
 		
-		System.out.println("1. select menu");
-		System.out.println("2. delete menu");
-		System.out.println("3. select side");
-		System.out.println("4. select juice");
+		int a = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(a != 5) {
+			System.out.println("1. select menu");
+			System.out.println("2. delete menu");
+			System.out.println("3. select side");
+			System.out.println("4. select juice");
+			a = scanner.nextInt();
+		}
 		System.out.println("5. exit");
 	}
 }
