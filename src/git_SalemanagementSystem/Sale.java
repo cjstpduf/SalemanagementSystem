76ce6bd55c59c77c9b707284b8fdd79473bbbe6b
tpduf <tpduf@tpduf>
2023-04-02 22:59:37 +0900

    added.Sale.java

--- src/git_SalemanagementSystem/Sale.java
+++ src/git_SalemanagementSystem/Sale.java
@@ -3,18 +3,47 @@ package git_SalemanagementSystem;
 import java.util.Scanner;
 
 public class Sale{
+
 	public static void main(String[] args) {
-		
-		int a = 0;
-		Scanner scanner = new Scanner(System.in);
-		
-		while(a != 5) {
-			System.out.println("1. select menu");
-			System.out.println("2. delete menu");
-			System.out.println("3. select side");
-			System.out.println("4. select juice");
-			a = scanner.nextInt();
-		}
-		System.out.println("5. exit");
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("choice!!!!!: food, drink");
+        String a = input1.next();
+        
+        int b;
+        
+        switch (a)
+        {
+        case "food":
+        b = 1;
+        break;
+        
+        case "drink":
+        b = 2;
+        break;
+        
+        default:
+        b = 3;
+        }
+        
+        if (b == 1)
+        {
+           menu();
+        }
+        else if (b == 2)
+        {
+           juice();
+        }       
+     }
+	public static void juice() {
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("coke, sprite, fanta");
+        String d = input3.next();
+        System.out.println("you ordered "+d);
+	}
+	public static void menu() {
+		Scanner input2 = new Scanner(System.in);
+        System.out.println("kimbob, ramen, fried rice");
+        String c= input2.next();
+        System.out.println("you ordered "+c);
 	}
 }
